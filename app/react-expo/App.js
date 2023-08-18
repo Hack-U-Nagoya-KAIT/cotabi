d99b59c4c1d7c34e62d3df115c4c1049be38af06
Naoki-02 <smile.naonao@icloud.com>
2023-08-18 17:30:36 +0900

    react環境修正

--- app/react-expo/App.js
+++ app/react-expo/App.js
@@ -1,31 +1,49 @@
-import axios from 'axios';
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import UserScreen from './screens/UserScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [data, setData] = useState('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        //axios.getの後かっこの中身を毎回変更する
-        const response = await axios.get('https://8a1f-2400-4051-cfe0-6600-3568-209e-6b99-456a.ngrok-free.app/api/data');
-        setData(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <Text>Data from Spring Boot: {data}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>{/* アプリ起動最初の画面設定 */}
+        <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{title: 'ホーム画面'}} />
+        <Stack.Screen
+        name="User"
+        component={UserScreen}
+        options={{title: 'ユーザ画面',headerShown: true}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+  // const [data, setData] = useState('');
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       //axios.getの後かっこの中身を毎回変更する
+  //       const response = await axios.get('https://8a1f-2400-4051-cfe0-6600-3568-209e-6b99-456a.ngrok-free.app/api/data');
+  //       setData(response.data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>Data from Spring Boot: {data}</Text>
+  //     <StatusBar style="auto" />
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
