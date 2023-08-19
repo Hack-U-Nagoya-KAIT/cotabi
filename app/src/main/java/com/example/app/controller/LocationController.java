5be524458e2f678df6a319e81f49743a1098588e
Naoki-02 <smile.naonao@icloud.com>
2023-08-19 18:35:30 +0900

    位置情報取得&YOLP_apiからの情報取得確認

---
+++ app/src/main/java/com/example/app/controller/LocationController.java
@@ -0,0 +1,26 @@
+package com.example.app.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.app.model.LocationRequest;
+
+@RestController
+public class LocationController {
+    @RequestMapping("/api")
+    public String hello() { // 追加
+		return "Hello World"; // 追加
+	} // 追加
+    @PostMapping("/api/location")
+    public String postLocation(@RequestBody LocationRequest request) {
+        double latitude = request.getLatitude();
+        double longitude = request.getLongitude();
+
+        // ここで必要な処理を行う（データベースへの保存など）
+        String yolpData =Yolp.getYolpData(latitude, longitude);
+
+        return "位置情報が受信されました。"+latitude+" "+longitude+yolpData;
+    }
+}
---
+++ app/src/main/java/com/example/app/model/LocationRequest.java
@@ -0,0 +1,25 @@
+package com.example.app.model;
+
+public class LocationRequest {
+    private double latitude;
+    private double longitude;
+
+    // コンストラクタ、ゲッター、セッターなどを定義
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
+
