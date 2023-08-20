41edb7bd5f7cd0513f95588a7e9d471e93e38ebe
Naoki-02 <smile.naonao@icloud.com>
2023-08-20 22:04:50 +0900

    修正

--- app/src/main/java/com/example/app/controller/Yolp.java
+++ app/src/main/java/com/example/app/controller/Yolp.java
@@ -9,14 +9,38 @@ public class Yolp {
         String apiKey = "dj00aiZpPWNvY0Z0MnpFaEJkWiZzPWNvbnN1bWVyc2VjcmV0Jng9N2Q-"; // ここに実際のAPIキーを入力
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity(
+        ResponseEntity<String> response_1 = restTemplate.getForEntity(
             baseUrl + "?appid=" + apiKey + "&device=mobile&lat="+latitude+"&lon="+longitude+"&dist=10&results=20&sort=geo&detail=string&gc=0302003",
             //"&dist=10&results=20&sort=geo&detail=string&gc=0204001&gc=0302003&gc=0303&gc=0305"
             String.class
         );
 
-        String responseBody = response.getBody();
-        System.out.println(responseBody);
-        return responseBody;
+        ResponseEntity<String> response_2 = restTemplate.getForEntity(
+            baseUrl + "?appid=" + apiKey + "&device=mobile&lat="+latitude+"&lon="+longitude+"&dist=10&results=20&sort=geo&detail=string&gc=0204001",
+            //"&dist=10&results=20&sort=geo&detail=string&gc=0204001&gc=0302003&gc=0303&gc=0305"
+            String.class
+        );
+
+        ResponseEntity<String> response_3 = restTemplate.getForEntity(
+            baseUrl + "?appid=" + apiKey + "&device=mobile&lat="+latitude+"&lon="+longitude+"&dist=10&results=20&sort=geo&detail=string&gc=0302003",
+            //"&dist=10&results=20&sort=geo&detail=string&gc=0204001&gc=0302003&gc=0303&gc=0305"
+            String.class
+        );
+
+        ResponseEntity<String> response_4 = restTemplate.getForEntity(
+            baseUrl + "?appid=" + apiKey + "&device=mobile&lat="+latitude+"&lon="+longitude+"&dist=10&results=20&sort=geo&detail=string&gc=0204001",
+            //"&dist=10&results=20&sort=geo&detail=string&gc=0204001&gc=0302003&gc=0303&gc=0305"
+            String.class
+        );
+
+        String response1Body = response_1.getBody();
+        System.out.println(response1Body);
+        String response2Body = response_2.getBody();
+        System.out.println(response2Body);
+        String response3Body = response_1.getBody();
+        System.out.println(response3Body);
+        String response4Body = response_2.getBody();
+        System.out.println(response4Body);
+        return response1Body+response2Body+response3Body+response4Body;
     }
 }
