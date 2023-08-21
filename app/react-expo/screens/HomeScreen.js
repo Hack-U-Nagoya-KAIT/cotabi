d4d2b6e99f2f3129a45b7e155e51919fbdd9bb6a
SaetoIto <saetohack@gmail.com>
2023-08-21 13:05:43 +0900

    フッター追加

--- app/react-expo/screens/HomeScreen.js
+++ app/react-expo/screens/HomeScreen.js
@@ -1,15 +1,49 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const FooterButton = ({ title, onPress }) => (
+  <TouchableOpacity style={styles.footerButton} onPress={onPress}>
+    <Text style={styles.footerButtonText}>{title}</Text>
+  </TouchableOpacity>
+);
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>ホーム画面</Text>
-      <Button title="ユーザ画面" onPress={() => navigation.navigate('User')} />
-      <Button title="履歴画面" onPress={() => navigation.navigate('History')} />
-      <Button title="設定画面" onPress={() => navigation.navigate('Setting')} />
+      {/* フッター */}
+      <View style={styles.footer}>
+        <FooterButton title="ユーザ画面" onPress={() => navigation.navigate('User')} />
+        <FooterButton title="履歴画面" onPress={() => navigation.navigate('History')} />
+        <FooterButton title="設定画面" onPress={() => navigation.navigate('Setting')} />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: 'white',
+    borderTopWidth: 1,
+    borderColor: '#ddd',
+  },
+  footerButton: {
+    paddingVertical: 10,
+  },
+  footerButtonText: {
+    fontSize: 16,
+  },
+});
 
 export default HomeScreen;
