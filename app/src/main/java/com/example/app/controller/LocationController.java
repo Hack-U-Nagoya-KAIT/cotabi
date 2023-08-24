23b09d92d132cb67a8d091bdde4fbb1393842c73
Naoki-02 <smile.naonao@icloud.com>
2023-08-24 22:58:18 +0900

    一店舗に絞れるようにした

--- app/src/main/java/com/example/app/controller/LocationController.java
+++ app/src/main/java/com/example/app/controller/LocationController.java
@@ -1,5 +1,10 @@
 package com.example.app.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,83 +12,62 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.app.SetDistance.SetDistance;
 import com.example.app.model.LocationRequest;
+import com.example.app.model.YolpData;
+import com.example.app.service.ChatGptServiceTag;
+import com.example.app.service.ContentNameParsing;
+import com.example.app.service.JsonParsing;
+import com.example.app.service.ListSplit;
+import com.example.app.service.RandomName;
 import com.example.app.service.Yolp;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+
 @RestController
 public class LocationController {
-    
+
     @RequestMapping("/api")
     public String hello() {
         return "Hello World";
     }
 
     @PostMapping("/api/location")
-    public String postLocation(@RequestBody LocationRequest request) throws JsonMappingException, JsonProcessingException{
-        double latitude = request.getLatitude();
-        double longitude = request.getLongitude();
-        //int time = request.getTime();
-        //int budget = request.getBudget();
-        //String concept = request.getConcept();
+    public ResponseEntity<Map<String, Object>> postLocation(@RequestBody LocationRequest request) throws JsonMappingException, JsonProcessingException {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            double latitude = request.getLatitude();
+            double longitude = request.getLongitude();
+            int time = request.getTime();
+            int budget = request.getBudget();
+            String concept = request.getConcept();
 
-        SetDistance setDistance = new SetDistance();
-        request.setBudget(2050);
-        int budget= request.getBudget();
-        
-        double distance = setDistance.GenerateDistance(budget);
+            SetDistance setDistance = new SetDistance();
 
-        // ここで必要な処理を行う（データベースへの保存など）
-        String taglist= Yolp.getYolpData(latitude , longitude , distance);
-        
-        // String json=ChatGptServiceTag.generateTravelSuggestion(taglist);
-        // System.out.println(json);
-        // String content = JsonParsing.json(json);
-        return "位置情報が受信されました。"+latitude+" "+longitude;
+            double distance = setDistance.GenerateDistance(budget);
+
+            // ここで必要な処理を行う（データベースへの保存など）
+            List<YolpData> dataList= Yolp.getYolpData(latitude, longitude, distance);
+            String taglist = ListSplit.Split(dataList);
+            System.out.println(taglist);
+            String json=ChatGptServiceTag.generateTravelSuggestion(taglist,concept);
+            System.out.println(json);
+            String content = JsonParsing.json(json);
+            System.out.println(content);
+            String result = ContentNameParsing.NameParsing(dataList,content);
+            System.out.println(result);
+            String spot = RandomName.random();
+            System.out.println(spot);
+
+
+
+            response.put("success", true);
+            response.put("message", "位置情報とデータが正常に受信されました。");
+            return ResponseEntity.ok(response);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.put("success", false);
+            response.put("message", "サーバーでエラーが発生しました。");
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 }
-
-
-
-
-// package com.example.app.controller;
-
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-
-// import com.example.app.SetDistance.SetDistance;
-// import com.example.app.model.LocationRequest;
-// import com.example.app.service.Yolp;
-// @RestController
-// public class LocationController {
-//     @RequestMapping("/api")
-//     public String hello() { // 追加
-//         return "Hello World"; // 追加
-//     } // 追加
-//     @PostMapping("/api/location")
-//     public String postLocation(@RequestBody LocationRequest request) {
-//         // double latitude = request.getLatitude();
-//         // double longitude = request.getLongitude();
-//         int time = request.getTime();
-//         int budget = request.getBudget();
-//         String concept = request.getConcept();
-//         System.out.println(time);
-//         System.out.println(budget);
-//         System.out.println(concept);
-//         // SetDistance setDistance = new SetDistance();
-//         // request.setBudget(2050);
-//         // int budget= request.getBudget();
-        
-//         // double distance = setDistance.GenerateDistance(budget);
-
-//         // // ここで必要な処理を行う（データベースへの保存など）
-//         // Yolp.getYolpData(latitude , longitude , distance);
-
-//         return "位置情報が受信されました。";
-//         // +latitude+" "+longitude;
-//     }
-// }
-
-
-
---
+++ app/src/main/java/com/example/app/service/JsonParsingYolp.java
@@ -0,0 +1,53 @@
+package com.example.app.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.app.model.YolpData;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonParsingYolp {
+    public static List<YolpData> json(String... jsonDataArray) {
+        List<YolpData> dataList = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        for (String jsonData : jsonDataArray) {
+            try {
+                JsonNode rootNode = objectMapper.readTree(jsonData);
+
+                JsonNode featureArrayNode = rootNode.get("Feature");
+                if (featureArrayNode.isArray()) {
+                    for (JsonNode featureNode : featureArrayNode) {
+                        JsonNode propertyNode = featureNode.get("Property");
+                        JsonNode geometryNode = featureNode.get("Geometry");
+
+                        String name = featureNode.get("Name").asText();
+                        String tag = propertyNode.get("Genre").get(0).get("Name").asText();
+
+                        String coordinates = geometryNode.get("Coordinates").asText();
+
+                        // Coordinatesを分割してlatとlonを取得
+                        String[] coordParts = coordinates.split(",");
+                        double lat = Double.parseDouble(coordParts[1]);
+                        double lon = Double.parseDouble(coordParts[0]);
+
+                        // YolpDataオブジェクトを作成してリストに追加
+                        YolpData data = new YolpData(name, lat, lon, tag);
+                        dataList.add(data);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        // dataListの内容を表示
+        for (YolpData data : dataList) {
+            System.out.println("Name: " + data.getName());
+            System.out.println("lat: " + data.getLat());
+            System.out.println("lon: " + data.getLon());
+            System.out.println("tag: " + data.getTag());
+        }
+        return dataList;
+    }
+}
