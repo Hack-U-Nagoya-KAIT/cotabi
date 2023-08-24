330e4985d8938e23009a9f87d5cbeeec12e3148e
Naoki-02 <smile.naonao@icloud.com>
2023-08-24 23:38:54 +0900

    修正

--- app/src/main/java/com/example/app/service/ContentNameParsing.java
+++ app/src/main/java/com/example/app/service/ContentNameParsing.java
@@ -4,28 +4,6 @@ import java.util.List;
 
 import com.example.app.model.YolpData;
 
-// public class ContentNameParsing {
-//     public static void main(List<YolpData> dataList) {
-//         String content = "ゲームセンター,デパート,遊園地"; // カンマで区切られた複数のタグ
-
-//         String[] tags = content.split(",");
-//         StringBuilder concatenatedNames = new StringBuilder();
-
-//         for (YolpData data : dataList) {
-//             for (String tag : tags) {
-//                 if (data.getTag().equals(tag.trim())) {
-//                     concatenatedNames.append(data.getName()).append(", ");
-//                 }
-//             }
-//         }
-
-//         // 最後のコンマとスペースを削除して表示
-//         String result = concatenatedNames.toString().replaceAll(", $", "");
-//         System.out.println("Names matching tags '" + content + "': " + result);
-//     }
-// }
-
-
 public class ContentNameParsing{
     public static String NameParsing(List<YolpData> dataList, String content) {
 
